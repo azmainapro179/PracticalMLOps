4eaf9e8f654a0bf80956f3918391145d97002a74
azapro01 <114428161+azapro01@users.noreply.github.com>
2026-01-11 20:41:14 +0600

    Did some changes

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,5 @@
 from hello import add
+from hello import subtract
 
 
 def test_add():
@@ -6,3 +7,8 @@ def test_add():
     assert 10 == add(7, 3)
     assert 2 == add(4, -2)
     assert -1 == add(-1, 0)
+
+
+def test_subtract():
+    assert 1 == subtract(5, 4)
+    assert -1 == subtract(2, 3)
